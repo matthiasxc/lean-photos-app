c11a5519caeaaefa149ad35e323b3b06ee830c0c
Matthias Shapiro <matthias.shapiro@gmail.com>
2024-03-20 12:42:16 -0400

    Daily tracking

--- src/TextSearch/TextSearch/ViewModels/MainViewModel.cs
+++ src/TextSearch/TextSearch/ViewModels/MainViewModel.cs
@@ -10,6 +10,7 @@ using System.Text;
 using System.Threading.Tasks;
 using Windows.Storage;
 using Windows.Storage.AccessCache;
+using Windows.Storage.Streams;
 
 namespace TextSearch.ViewModels
 {
@@ -25,11 +26,80 @@ namespace TextSearch.ViewModels
             Initialize();
         }
 
-        private string _searchString = "";
+        private string _searchString = "zira";
         public string SearchString
         {
             get { return _searchString; }
             set { Set(ref _searchString, value); }
+        }
+        private string _searchString2 = "404,906,6275";
+        public string SearchString2
+        {
+            get { return _searchString2; }
+            set { Set(ref _searchString2, value); }
+        }
+        private string _searchString3 = "770,313,1997";
+        public string SearchString3
+        {
+            get { return _searchString3; }
+            set { Set(ref _searchString3, value); }
+        }
+
+        private string _searchString4 = "770,928,7132";
+        public string SearchString4
+        {
+            get { return _searchString4; }
+            set { Set(ref _searchString4, value); }
+        }
+        private string _searchString5 = "770,988,6927";
+        public string SearchString5
+        {
+            get { return _searchString5; }
+            set { Set(ref _searchString5, value); }
+        }
+        private string _searchString6 = "404,434,2260";
+        public string SearchString6
+        {
+            get { return _searchString6; }
+            set { Set(ref _searchString6, value); }
+        }
+
+        private string _searchString7 = "404,409,9466";
+        public string SearchString7
+        {
+            get { return _searchString7; }
+            set { Set(ref _searchString7, value); }
+        }
+        private string _searchString8 = "404,929,9466";
+        public string SearchString8
+        {
+            get { return _searchString8; }
+            set { Set(ref _searchString8, value); }
+        }
+        private string _searchString9 = "404,754,3124";
+        public string SearchString9
+        {
+            get { return _searchString9; }
+            set { Set(ref _searchString9, value); }
+        }
+
+        private string _searchString10 = "770,457,2340";
+        public string SearchString10
+        {
+            get { return _searchString10; }
+            set { Set(ref _searchString10, value); }
+        }
+        private string _searchString11 = "404,986,9478";
+        public string SearchString11
+        {
+            get { return _searchString11; }
+            set { Set(ref _searchString11, value); }
+        }
+        private string _searchString12 = "770,807,6576";
+        public string SearchString12
+        {
+            get { return _searchString12; }
+            set { Set(ref _searchString12, value); }
         }
 
         private bool _isReadyToSearch = false;
@@ -87,7 +157,7 @@ namespace TextSearch.ViewModels
                     async () =>
                     {
                         fileCount = folderCount = iteration = 0;
-                        FoundFiles = await GetFilesInDirectory(TargetFolder);
+                        //FoundFiles = await GetFilesInDirectory(TargetFolder);
 
                         string rootPath = @"D:\utter faves\oldbadthings\websites\atlanta.backpage.com";
                         string searchText = @"zira";
@@ -100,13 +170,27 @@ namespace TextSearch.ViewModels
                         try
                         {
                             Debug.WriteLine($"Starting Search");
+                            List<string> searchTerms = new List<string>();
+                            if(SearchString.Length > 1 ) { searchTerms.Add(SearchString); }
+                            if (SearchString2.Length > 1) { searchTerms.Add(SearchString2); }
+                            if (SearchString3.Length > 1) { searchTerms.Add(SearchString3); }
+                            //if (SearchString4.Length > 1) { searchTerms.Add(SearchString4); }
+                            //if (SearchString5.Length > 1) { searchTerms.Add(SearchString5); }
+                            //if (SearchString6.Length > 1) { searchTerms.Add(SearchString6); }
+                            //if (SearchString7.Length > 1) { searchTerms.Add(SearchString7); }
+                            //if (SearchString8.Length > 1) { searchTerms.Add(SearchString8); }
+                            //if (SearchString9.Length > 1) { searchTerms.Add(SearchString9); }
+                            //if (SearchString10.Length > 1) { searchTerms.Add(SearchString10); }
+                            //if (SearchString11.Length > 1) { searchTerms.Add(SearchString11); }
+                            //if (SearchString12.Length > 1) { searchTerms.Add(SearchString12); }
+
                             foreach (var filePath in FoundFiles)
                             {
                                 iteration2++;
 
-                                if (iteration2 % 200 == 0)
+                                if (iteration2 % 500 == 0)
                                 {
-                                    StatusText = $"Searched {iteration2} files with {positiveFiles.Count} results";
+                                    StatusText = $"Searched {iteration2} files of {FoundFiles.Count} with {positiveFiles.Count} results";
                                     Debug.WriteLine(StatusText);
                                 }
                             // Optional: Check the file extension if you want to filter specific types
@@ -115,11 +199,14 @@ namespace TextSearch.ViewModels
                                 if (SearchString.Length < 3)
                                 {
                                     //(404)604 - 8612
-                                    SearchString = "464 - 7058";
+                                    SearchString = "4908";
                                 }
-                                if (await FileContainsText(filePath, SearchString) != "none")
+                                var searchResults = await FileContainsText(filePath, searchTerms);
+                                if (searchResults != "none")
                                 {
-                                    positiveFiles.Add(filePath);
+                                    positiveFiles.Add(searchResults);
+                                    StatusText = $"Found {SearchString} in {filePath} results";
+                                    Debug.WriteLine(StatusText);
                                 }
                                 // }
                             }
@@ -160,18 +247,53 @@ namespace TextSearch.ViewModels
         }
         #endregion
 
-        static async Task<string> FileContainsText(string filePath, string searchText)
+        static async Task<string> FileContainsText(string filePath, List<string> searchStrings)
         {
             try
             {
                 StorageFile sf = await StorageFile.GetFileFromPathAsync(filePath);
                 // Read the content of the file
-                var content = await FileIO.ReadTextAsync(sf);
+                //var content = await FileIO.ReadTextAsync(sf);
 
-                // Search the file content for the specified text
-                if (content.ToLower().Contains(searchText))
+                if (sf != null)
                 {
-                    return filePath;
+                    IBuffer buffer = await FileIO.ReadBufferAsync(sf);
+                    DataReader reader = DataReader.FromBuffer(buffer);
+                    byte[] fileContent = new byte[reader.UnconsumedBufferLength];
+                    reader.ReadBytes(fileContent);
+                    string text = Encoding.UTF8.GetString(fileContent, 0, fileContent.Length);
+                    // Search the file content for the specified text
+                    bool isFound = true;
+                    string searchTerm = "";
+                    foreach (string line in searchStrings)
+                    {
+                        isFound = true;
+                        string tempSearchTerm = "";
+
+                        // Concat
+                        List<string> multiSearchString = line.Split(',', StringSplitOptions.RemoveEmptyEntries).ToList();
+                        foreach(string term in multiSearchString)
+                        {
+                            if (!text.ToLower().Contains(term.ToLower()))
+                            {
+                                isFound = false;
+                            }
+                            else
+                            {
+                                tempSearchTerm += term+",";
+                            }
+                        }
+
+                        if (isFound)
+                        {
+                            searchTerm += tempSearchTerm + " ";
+                        }
+                    }
+
+                    if (searchTerm != "")
+                    {
+                        return searchTerm + filePath;
+                    }
                 }
             }
             catch (Exception ex)
